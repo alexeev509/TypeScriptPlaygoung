8bd74753524ec53f51e98126f48b8aefadad98b9
alexeev509 <32396885+alexeev509@users.noreply.github.com>
2024-05-23 19:42:40 +0300

    Update CondtionalTypes.ts
    example of trying to infer return type of any function

--- CondtionalTypes.ts
+++ CondtionalTypes.ts
@@ -14,3 +14,15 @@ type TryInfer<T extends object = object> = T extends infer R ? R[keyof R]: null
 type R = TryInfer<{a: 1, b: 2}>
 
 const rr: R = 2
+
+
+// example of trying to infer return type of any function
+function test () {
+  return 2
+}
+
+type FunctionResult<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any
+
+type Q = FunctionResult<typeof test>
+
+const anyNumber: Q = 555
